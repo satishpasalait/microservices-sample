993b3bb5b084614dd2a686e9e60ca5af369a1e19
Satish Pasala <satishpasalait@gmail.com>
2025-11-25 12:56:11 -0500

    Added AutoMapper profile.

--- src/Services/Order/Microservices.Sample.Order.Application/Mappings/MappingProfile.cs
+++ src/Services/Order/Microservices.Sample.Order.Application/Mappings/MappingProfile.cs
@@ -1,6 +1,16 @@
-﻿namespace Microservices.Sample.Order.Application.Mappings
+﻿using AutoMapper;
+using Microservices.Sample.Order.Application.DTOs;
+using OrderEntity = Microservices.Sample.Order.Domain.Entities.Order;
+using OrderItemEntity = Microservices.Sample.Order.Domain.Entities.OrderItem;
+
+namespace Microservices.Sample.Order.Application.Mappings
 {
-    public class MappingProfile
+    public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<OrderEntity, OrderDto>();
+            CreateMap<OrderItemEntity, OrderItemDto>();
+        }
     }
 }
